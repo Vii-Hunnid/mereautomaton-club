23a1523ec4b70e42ef2b470449db5dfc13fa586b
viihunnid <viihunnid@icloud.com>
2025-08-11 23:47:41 +0200

    Retry

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
       fontFamily: {
@@ -18,8 +17,4 @@ export default {
       }
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
-  ],
 }
